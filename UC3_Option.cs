254644ced1d2c9f47c184139c35c17f4ca7a7bbe
“Salil48” <salilchharia@gmail.com>
2022-12-30 15:53:37 +0530

    The Player then checks for a Option. They are No Play, Ladder or Snake

---
+++ UC3_Option.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadder
+{
+    public class UC3_Option
+    {
+        public static void Game()
+        {
+            int position = 0;
+            Random random = new Random();
+            int die = random.Next(1, 7);
+            int opt = random.Next(3);
+            const int NoPlay = 0;
+            const int ladder = 1;
+            const int snake = 2;
+            switch (opt)
+            {
+
+                case NoPlay:
+                    break;
+                case ladder:
+                    position = die;
+                    break;
+                case snake:
+                    position = position - die;
+                    break;
+            }
+            Console.WriteLine("die: " + die + " option : " + opt + " position : " + position);
+
+
+
+        }
+    }
+}
