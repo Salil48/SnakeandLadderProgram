588cd8b2732d646ac229eb58014e84f684f67f1a
“Salil48” <salilchharia@gmail.com>
2022-12-30 15:43:04 +0530

    Snake and Ladder game played with single player at start position 0

---
+++ UC1_Start.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadder
+{
+    public class UC1_Start
+    {
+        public static void Game()
+        {
+            int startPosition = 0;
+            Console.WriteLine("Snake and Ladder game played with single player at start position : " + startPosition);
+
+        }
+    }
+}
